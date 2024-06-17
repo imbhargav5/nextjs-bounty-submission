0653f58bb817631072b7e2b605f5c31366248c52
Bhargav Ponnapalli <imbhargav5@gmail.com>
2024-06-17 16:49:21 +0530

    init

---
+++ .eslintrc.js
@@ -0,0 +1,77 @@
+var tsConfigs = ['./tsconfig.json'];
+var tsConfigEmail = ['./tsconfig-emails.json'];
+
+var srcRuleOverrides = {
+  'prettier/prettier': 1,
+  '@typescript-eslint/no-unused-vars': 1,
+  '@typescript-eslint/no-non-null-assertion': 'error',
+};
+
+module.exports = {
+  overrides: [
+    {
+      extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier',
+        'plugin:@next/next/recommended',
+      ],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: tsConfigs,
+      },
+      plugins: ['@typescript-eslint', 'prettier'],
+      rules: srcRuleOverrides,
+      files: ['src/**/*.ts', 'src/**/*.tsx'],
+    },
+    {
+      extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier',
+        'plugin:@next/next/recommended',
+      ],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: tsConfigEmail,
+      },
+      plugins: ['@typescript-eslint', 'prettier'],
+      rules: srcRuleOverrides,
+      files: ['emails/**/*.ts', 'emails/**/*.tsx'],
+    },
+    {
+      extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier',
+      ],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: tsConfigs,
+      },
+      plugins: [
+        '@typescript-eslint',
+        'plugin:playwright/playwright-test',
+        'prettier',
+      ],
+      rules: srcRuleOverrides,
+      files: ['e2e/**/*.ts'],
+    },
+
+    {
+      extends: ['eslint:recommended', 'prettier'],
+      files: '*.mjs',
+    },
+    // make nextconfig.mjs node environment
+    {
+      extends: [
+        'eslint:recommended',
+        'prettier',
+        'plugin:@next/next/recommended',
+      ],
+      files: 'next.config.mjs',
+    },
+  ],
+  extends: ['plugin:@next/next/recommended'],
+  root: true,
+};
